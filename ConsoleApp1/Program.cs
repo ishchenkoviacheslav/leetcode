65e17d5927333fc688dcabf0c45bddc15e06862d
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-03-11 16:25:44 +0100

    successful

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,33 +1,9 @@
 ﻿class Program
 {
-    static public int RemoveDuplicates(int[] nums)
-    {
-        var oneOrTwoDuplicates = new List<int>();
-        int temp = int.MinValue;
-        int countOfAlreadyAdded = 0;
-        for (int i = 0; i < nums.Length; i++)
-        {
-            if (temp != nums[i])
-            {
-                countOfAlreadyAdded = 0;
-            }
-            if (countOfAlreadyAdded < 2)
-            {
-                temp = nums[i];
-                countOfAlreadyAdded++;
-                oneOrTwoDuplicates.Add(nums[i]);
-            }
-        }
-        for (int i = 0; i < oneOrTwoDuplicates.Count; i++)
-        {
-            nums[i] = oneOrTwoDuplicates[i];
-        }
-        return oneOrTwoDuplicates.Count;
-    }
+    
 
     static void Main()
     {
-        int ress = RemoveDuplicates(new int[] { 2, 4, 1 });
-        Console.WriteLine(ress);
+        
     }
 }
---
+++ ConsoleApp1/leetcode/array_string/RotateArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.array_string
+{
+    internal class RotateArray
+    {
+        static public void Rotate(int[] nums, int k)
+        {
+            if (nums.Length == 1)
+            {
+                return;
+            }
+            if (k > nums.Length)
+            {
+                while (true)
+                {
+                    k = k - nums.Length;
+                    if (k < nums.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+            int[] clone = new int[nums.Length];
+
+            for (int i = 0; i < k; i++)
+            {
+                clone[i] = nums[nums.Length - k + i];
+            }
+            for (int i = 0; i < nums.Length - k; i++)
+            {
+                clone[k + i] = nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = clone[i];
+            }
+        }
+    }
+}
