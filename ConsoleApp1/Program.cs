405bab23718637baab0eb1795be6059211314a67
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-03-14 05:36:04 +0100

    balancer in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,26 +10,62 @@
     static bool IsBalanced(string s)
     {
         var chars = s.ToCharArray();
-        int balancer = 0;
+        int balancerRound = 0;
+        int balancerCurve = 0;
+        int balancerSquare = 0;
         for (int i = 0; i < chars.Length; i++)
         {
-            if (chars[i] == '(')
+            if (chars[i] == '(' || chars[i] == ')')
             {
-                balancer++;
+                if (chars[i] == '(')
+                {
+                    balancerRound++;
+                }
+                if (chars[i] == ')')
+                {
+                    balancerRound--;
+                }
             }
-            else if (chars[i] == ')')
+            //***************************************
+            if (chars[i] == '{' || chars[i] == '}')
             {
-                balancer--;
+                if (chars[i] == '{')
+                {
+                    balancerCurve++;
+                }
+                if (chars[i] == '}')
+                {
+                    balancerCurve--;
+                }
+            }
+            //***************************************
+            if (chars[i] == '[' || chars[i] == ']')
+            {
+                if (chars[i] == '[')
+                {
+                    balancerSquare++;
+                }
+                if (chars[i] == ']')
+                {
+                    balancerSquare--;
+                }
             }
 
-            return balancer < 0;
+            if (balancerRound < 0 || balancerCurve < 0 || balancerSquare < 0)
+            {
+                return false;
+            }
         }
 
-        return balancer == 0;
+        return balancerRound == 0 &&balancerCurve == 0 &&balancerSquare == 0;
     }
     static void Main()
     {
-        Console.WriteLine(IsBalanced("((()))"));
-        Console.WriteLine(IsBalanced(")("));
+        //Console.WriteLine(IsBalanced("((()))")); //T
+        //Console.WriteLine(IsBalanced("({[]})")); //T
+        //Console.WriteLine(IsBalanced("([)"));//F
+        Console.WriteLine(IsBalanced("([)]")); //F
+        //Console.WriteLine(IsBalanced("([])")); //T
+        //Console.WriteLine(IsBalanced(")(")); //F
     }
 }
