352c8ea556cf2746d9fd538bb64edc866ab5deb8
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-03-17 15:30:24 +0100

    Length of Last Word finished

---
+++ ConsoleApp1/leetcode/array_string/LengthofLastWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.array_string
+{
+    internal class LengthofLastWord
+    {
+        static public int LengthOfLastWord(string s)
+        {
+            char[] chars = s.Trim().ToCharArray();
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                if (chars[i] == ' ')
+                {
+                    return chars.Length - 1 - i;
+                }
+            }
+
+            return chars.Length;
+        }
+    }
+}
