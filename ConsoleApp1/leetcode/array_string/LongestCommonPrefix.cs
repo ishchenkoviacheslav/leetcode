bd5d7bc8970ca09d6d7555b48185a188e383fc79
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-03-17 23:39:31 +0100

    longest common prefix finished

---
+++ ConsoleApp1/leetcode/array_string/LongestCommonPrefix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.array_string
+{
+    internal class LongestCommonPrefix
+    {
+        static public string LongestCommonPrefix(string[] strs)
+        {
+            List<char> prefix = strs[0].ToCharArray().ToList();
+            int longOfTheShortestArray = int.MaxValue;
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i].Length < longOfTheShortestArray)
+                {
+                    longOfTheShortestArray = strs[i].Length;
+                }
+                for (int j = 0; j < strs[i].Length; j++)
+                {
+                    if (prefix.Count <= j)
+                    {
+                        break;
+                    }
+                    if (prefix[j] != strs[i][j])
+                    {
+                        prefix.RemoveRange(j, prefix.Count - j);
+                    }
+                }
+            }
+            return new string(prefix.Take(longOfTheShortestArray).ToArray());
+        }
+    }
+}
