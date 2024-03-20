e202e52983713cb668e0801c1431648eab96f629
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-03-21 00:32:47 +0100

    Fibonacci

---
+++ ConsoleApp1/interviews/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1.interviews
+{
+    internal class Fibonacci
+    {
+        static public int Fib(int x)
+        {
+            int i = 1;
+            int first = 0;
+            int second = 1;
+            while (true)
+            {
+                if (i == x)
+                {
+                    return first;
+                }
+
+                int temp = first + second;
+                first = second;
+                second = temp;
+
+                i++;
+            }
+        }
+        //Console.WriteLine(Fib(5)); //3
+        //Console.WriteLine(Fib(6)); //5
+        //Console.WriteLine(Fib(9)); //21
+        //Console.WriteLine(Fib(10)); //34
+    }
+}
