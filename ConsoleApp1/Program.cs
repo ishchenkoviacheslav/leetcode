a5b3e27713ec11294c67bdf771e2f388a16edb47
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-18 06:55:57 +0200

    in progress gas station

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,41 +6,58 @@
         int index = 0;
         int lastIndex = gas.Length - 1;
         int tries = 0;
-        int gasVolume = 0;
+        int tank = 0;
+        bool firstInit = true;
         while (true)
         {
-            gasVolume = gasVolume + gas[index] - cost[index];
-            if (gasVolume > 0)
+            if (gas[index] - cost[index] < 1 && index == startIndex)
             {
+                tank = -1;
+            }
+            else
+            {
+                if (firstInit)
+                {
+                    firstInit = false;
+                    tank = gas[index];
+                }
+                else
+                {
+                    tank = tank - cost[index-1 < 0 ? lastIndex : index-1] + gas[index];
+                }
+            }
+
+            if (tank > 0)
+            {
+                if (startIndex+1 == index)
+                {
+                    return startIndex;
+                }
+
                 if (index == lastIndex)
                 {
                     index = 0;
-                    gasVolume += gas[index];
                 }
                 else
                 {
                     index++;
-                    gasVolume += gas[index];
-                }
-
-                if (startIndex == index)
-                {
-                    return startIndex;
                 }
             }
-            else if (gasVolume < 1)
+            else if (tank < 1)
             {
                 if (index == lastIndex)
                 {
                     startIndex = 0;
                     index = 0;
-                    gasVolume = 0;
+                    tank = 0;
+                    firstInit = true;
                 }
                 else
                 {
                     startIndex++;
                     index++;
-                    gasVolume = 0;
+                    tank = 0;
+                    firstInit = true;
                 }
                 tries++;
             }
@@ -54,6 +71,7 @@
     static void Main()
     {
         //Console.WriteLine(CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }));
-        Console.WriteLine(CanCompleteCircuit(new int[] { 2, 3, 4 }, new int[] { 3, 4, 3 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 2, 3, 4 }, new int[] { 3, 4, 3 }));
+        Console.WriteLine(CanCompleteCircuit(new int[] { 5, 1, 2, 3, 4 }, new int[] { 4, 4, 1, 5, 1 }));
     }
 }
