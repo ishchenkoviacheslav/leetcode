04ef16dbff3fe710272f4b92ccc7a36d352de906
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-18 20:55:37 +0200

    interview question added

---
+++ ConsoleApp1/interviews/TaskAwaitWhenAll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.interviews
+{
+    internal class TaskAwaitWhenAll
+    {
+        static async Task My()//Main
+        {
+            await Task.Run(async () =>
+            {
+                await Task.Delay(10000);
+                await Console.Out.WriteLineAsync("10 000");
+            });
+
+            await Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                await Console.Out.WriteLineAsync("5 000");
+            });
+
+            Console.WriteLine("end one");
+            ///////////////////////////////////////////////////////////
+            Task t1 = Task.Run(async () =>
+            {
+                await Task.Delay(10000);
+                await Console.Out.WriteLineAsync("10 000");
+            });
+
+            Task t2 = Task.Run(async () =>
+            {
+                await Task.Delay(5000);
+                await Console.Out.WriteLineAsync("5 000");
+            });
+
+            Console.WriteLine("end two");
+
+            await Task.WhenAll(t1, t2);
+        }
+    }
+}
