10e571cab2ad36e0f858c4e8d6169ad9e663fa89
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-19 23:08:35 +0200

    in progresss

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@
         {
             tank = tank + gas[index] - cost[index];
 
-            if (tank > 0 || (tank == 0 && startIndex == index+1))
+            if (tank > 0 || (tank == 0 && startIndex == (index+1 > lastIndex ? 0 : index+1)))
             {
                 if (startIndex == index && firstInit == false)
                 {
@@ -42,10 +42,11 @@
     }
     static void Main()
     {
-        Console.WriteLine(CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }));
-        Console.WriteLine(CanCompleteCircuit(new int[] { 2, 3, 4 }, new int[] { 3, 4, 3 }));
-        Console.WriteLine(CanCompleteCircuit(new int[] { 5, 1, 2, 3, 4 }, new int[] { 4, 4, 1, 5, 1 }));
-        Console.WriteLine(CanCompleteCircuit(new int[] { 3, 3, 4 }, new int[] { 3, 4, 4 }));
-        Console.WriteLine(CanCompleteCircuit(new int[] { 4, 5, 2, 6, 5, 3 }, new int[] { 3, 2, 7, 3, 2, 9 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 1, 2, 3, 4, 5 }, new int[] { 3, 4, 5, 1, 2 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 2, 3, 4 }, new int[] { 3, 4, 3 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 5, 1, 2, 3, 4 }, new int[] { 4, 4, 1, 5, 1 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 3, 3, 4 }, new int[] { 3, 4, 4 }));
+        //Console.WriteLine(CanCompleteCircuit(new int[] { 4, 5, 2, 6, 5, 3 }, new int[] { 3, 2, 7, 3, 2, 9 }));
+        Console.WriteLine(CanCompleteCircuit(new int[] { 3, 1, 1 }, new int[] { 1, 2, 2 }));
     }
 }
