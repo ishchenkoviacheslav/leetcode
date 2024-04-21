3fae5691149850833eb617de5b6b5147e563ddd8
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-21 22:09:32 +0200

    in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,29 @@ class Program
 {
     static public string Convert(string s, int numRows)
     {
-        return string.Empty;
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < rows.Length; i++)
+        {
+            rows[i] = new StringBuilder();
+        }
+        var asChars = s.ToCharArray();
+
+        for (int i = 0; i < asChars.Length; i++)
+        {
+            rows[j].Append(asChars[i]);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < rows.Length; i++)
+        {
+            result.Append(rows[i]);
+        }
+
+        return result.ToString();
     }
     static void Main()
     {
-       
+        Console.WriteLine(Convert("PAYPALISHIRING", 3));
+        Console.WriteLine(Convert("PAYPALISHIRING", 4));
     }
 }
