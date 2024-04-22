ec66d3f0b6f210af960d18cd729ef95b3103b928
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-22 14:11:54 +0200

    in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,50 @@
 {
     static public int Candy(int[] ratings)
     {
-        return 0;
+        int totalCandies = 0;
+        int currentCandies = 1;
+        for (int i = 0; i < ratings.Length; i++)
+        {
+            if (i == 0)
+            {
+                if (ratings[i] > ratings[i + 1])
+                {
+                    currentCandies++;
+                }
+                totalCandies += currentCandies;
+                continue;
+            }
+            if (i == ratings.Length - 1)
+            {
+                if (ratings[i] <= ratings[i - 1] && currentCandies > 1)
+                {
+                    currentCandies--;
+                }
+                else if (ratings[i] > ratings[i - 1])
+                {
+                    currentCandies++;
+                }
+                totalCandies += currentCandies;
+
+                continue;
+            }
+
+            if (ratings[i] > ratings[i + 1] || ratings[i] > ratings[i - 1])
+            {
+                currentCandies++;
+            }
+            else if (ratings[i] <= ratings[i + 1] && currentCandies > 1)
+            {
+                currentCandies--;
+            }
+            totalCandies += currentCandies;
+        }
+
+        return totalCandies;
     }
     static void Main()
     {
-        Console.WriteLine(Candy(new []{ 1,0,2}));
+        Console.WriteLine(Candy(new []{ 1,0,2}));       //5
+        Console.WriteLine(Candy(new []{ 1, 2, 2 }));    //4
     }
 }
