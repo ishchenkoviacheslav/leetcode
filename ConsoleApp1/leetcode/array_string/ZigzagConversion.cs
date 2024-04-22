9fef591fd841bc92b00bb5ec18fee6ce7e60b42c
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-22 11:53:39 +0200

    zigzag finished

--- ConsoleApp1/leetcode/array_string/ZigzagConversion.cs
+++ ConsoleApp1/leetcode/array_string/ZigzagConversion.cs
@@ -8,5 +8,46 @@ namespace ConsoleApp1.leetcode.array_string
 {
     internal class ZigzagConversion
     {
+        public string Convert(string s, int numRows)
+        {
+            StringBuilder[] rows = new StringBuilder[numRows];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                rows[i] = new StringBuilder();
+            }
+
+            Queue<char> asChars = new Queue<char>(s.ToCharArray());
+            int j = 0;
+            bool zigzagSymbols = false;
+            int counter = asChars.Count;
+            for (int i = 0; i < counter;)
+            {
+                if (zigzagSymbols && j < numRows - 1 && j > 0)
+                {
+                    rows[numRows - 1 - j].Append(asChars.Dequeue());
+                    i++;
+                }
+                else if (zigzagSymbols == false)
+                {
+                    rows[j].Append(asChars.Dequeue());
+                    i++;
+                }
+                j++;
+
+                if (j == numRows)
+                {
+                    zigzagSymbols = zigzagSymbols ? false : true;
+                    j = 0;
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < rows.Length; i++)
+            {
+                result.Append(rows[i]);
+            }
+
+            return result.ToString();
+        }
     }
 }
