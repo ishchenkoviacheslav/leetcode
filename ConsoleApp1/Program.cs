5de7a68f4ad316a75b6f703573215be9fa07aff6
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-28 02:39:13 +0200

    trapping rain water in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,14 +1,53 @@
-﻿using System.Diagnostics;
-
-class Program
+﻿class Program
 {
     static public int Trap(int[] height)
     {
-        return 0;
+        int leftPeek = int.MinValue;
+        int rightPeek = int.MinValue;
+        int notEmptyCurrentPeeks = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int waterTotal = 0;
+        bool firstPeekInit = true;
+
+        for (int i = 0; i < height.Length; i++)
+        {
+            if (height[i] > leftPeek && firstPeekInit)
+            {
+                leftPeek = height[i];
+                leftIndex = i;
+                firstPeekInit = false;
+                continue;
+            }
+            notEmptyCurrentPeeks += height[i];
+            //if equals?
+            if (height[i] > rightPeek)
+            {
+                rightPeek = height[i];
+                rightIndex = i;
+            }
+            else
+            {
+                notEmptyCurrentPeeks -= height[i];
+                notEmptyCurrentPeeks -= rightPeek;
+
+                int squareBetween = Math.Min(leftPeek, rightPeek) * ((rightIndex + 1 - leftIndex + 1) - 1);
+                waterTotal += squareBetween - notEmptyCurrentPeeks;
+
+                notEmptyCurrentPeeks = 0;
+
+                leftIndex = rightIndex;
+                leftPeek = rightPeek;
+                rightPeek = 0;
+                rightIndex = 0;
+            }
+        }
+
+        return waterTotal;
     }
 
     static async Task Main()
     {
-        //Console.WriteLine(Trap(new int[] {}));
+        Console.WriteLine(Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
     }
 }
