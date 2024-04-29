8da019e1859f7cd1a8c3c3b21bc48cca0aa32e90
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-29 10:53:12 +0200

    in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,24 +4,50 @@
     {
         //two rules how to deretmine if it's peak.
         //1 - if there is any peak which is heighest than first (or equals)
-        //just use this logic height[i] >= peekOneValue
+        //just use this logic height[i] >= peakOneValue
         //2 - if there is only peak which is not so high as first, but it's highest than latest
         //to be sure with this case, it's need to be checked to the end of the array
         //and if nothing found, than use the highest peak is found.
         //
         int totalNotEmptyArea = 0, totalArea = 0;
-        int peekOneIndex = 0, peekTwoIndex = 0, peekOneValue = 0, peekTwoValue = 0;
-        bool firstPeakLogic = true;
+        int peakOneIndex = -1, peakTwoIndex = -1, peakOneValue = 0, peakTwoValue = 0;
+        int heigherDescendingPeakValue = 0;
+        int heigherDescendingPeakIndex = -1;
+        bool firstPeakLogic = true, secondDescendingPeak = false;
         for (int i = 0; i < height.Length; i++)
         {
-            if (height[i] >= peekOneValue && firstPeakLogic)
+            if (height[i] >= peakOneValue && firstPeakLogic)
             {
-                peekOneValue = height[i];
-                peekOneIndex = i;
+                peakOneValue = height[i];
+                peakOneIndex = i;
             }
-            else
+            firstPeakLogic = false;
+            //looking for a raising peak
+            if(height[i] >= peakOneValue && secondDescendingPeak == false)
             {
-                firstPeakLogic = false;
+                peakTwoValue = height[i];
+                peakTwoIndex = i;
+            }
+            else if (i == height.Length - 1 && secondDescendingPeak == false)
+            {
+                secondDescendingPeak = true;
+                i = peakOneIndex;
+            }
+
+            if (height[i] > heigherDescendingPeakValue && secondDescendingPeak)
+            {
+                heigherDescendingPeakValue = height[i];
+                heigherDescendingPeakIndex = i;
+            } 
+            else if (i == height.Length - 1 && secondDescendingPeak)
+            {
+                peakTwoValue = heigherDescendingPeakValue;
+                peakTwoIndex = heigherDescendingPeakIndex;
+            }
+
+
+            if (firstPeakLogic == false && )
+            {
 
             }
         }
