e86af38fb2641a514ae0e649f959f0408408697e
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-30 19:56:19 +0200

    in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,40 @@
     static public IList<string> FullJustify(string[] words, int maxWidth)
     {
         var result = new Dictionary<int, string>();
-
         for (int i = 0, j = 0; i < words.Length; i++)
         {
-            result.Add(j, words[i]);
+            var lengthOfStoredWord = result.ContainsKey(j) ? result[j].Length + 1 : 0;
+
+            if (lengthOfStoredWord + words[i].Length < maxWidth)
+            {
+                if (result.ContainsKey(j))
+                {
+                    result[j] += " " + words[i];
+                }
+                else
+                {
+                    result.Add(j, words[i]);
+                }
+            }
+            else
+            {
+                j++;
+                i--;
+            }
         }
 
-        return result;
+        for (int i = 0; i < result.Count; i++)
+        {
+            var separatedWords = result[i].Split(" ", StringSplitOptions.TrimEntries);
+            var commonCountOfSymbols = separatedWords.Sum(s => s.Length);
+            var temp = string.Empty;
+            for (int j = 0; j < separated.Length; j++)
+            {
+
+            }
+        }
+
+        return result.Select(kvp => kvp.Value).ToList();
     }
     static async Task Main()
     {
