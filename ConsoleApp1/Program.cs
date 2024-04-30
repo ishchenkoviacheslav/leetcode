5f83ac05da0ee70f2467c498452b09cfe1c5af1f
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-04-30 13:46:31 +0200

    in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,21 @@
 {
     static public IList<string> FullJustify(string[] words, int maxWidth)
     {
+        var result = new Dictionary<int, string>();
 
+        for (int i = 0, j = 0; i < words.Length; i++)
+        {
+            result.Add(j, words[i]);
+        }
+
+        return result;
     }
     static async Task Main()
     {
-       
+        var result = FullJustify(new string[] { "This", "is", "an", "example", "of", "text", "justification." }, 16);
+        foreach (var item in result)
+        {
+            await Console.Out.WriteLineAsync(item);
+        }
     }
 }
