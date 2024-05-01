1344bebe10b9fe428e888d9ff1a42f909c9adb9a
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-05-01 19:15:46 +0200

    3Sum in progress

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,38 @@
 ﻿class Program
 {
+    static public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new Dictionary<string,List<int>>();
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            for (int j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                    {
+                        var orederedValues = new List<int> { nums[i], nums[j], nums[k] }.OrderBy(x => x).Select(x => x.ToString());
+                        var orederedKey = string.Join(string.Empty, orederedValues);
+                        if (!result.ContainsKey(orederedKey))
+                        {
+                            result[orederedKey] = new List<int> { nums[i], nums[j], nums[k] };
+                        }
+                    }
+                }
+            }
+        }
+
+        return result.Values.Cast<IList<int>>().ToList();
+    }
     static async Task Main()
     {
+        foreach (var items in ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 }))
+        {
+            foreach (var item in items)
+            {
+                await Console.Out.WriteAsync(item.ToString() + " ");
+            }
+            await Console.Out.WriteLineAsync();
+        }
     }
 }
---
+++ ConsoleApp1/leetcode/Two_Pointers/3Sum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.Two_Pointers
+{
+    internal class _3Sum
+    {
+    }
+}
