7a87dbcbee19bcb25bb88f4465f22fbd3931dd71
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-05-01 12:14:42 +0200

    TwoSum_II_InputArrayIsSorted finished

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,5 +2,6 @@
 {
     static async Task Main()
     {
+        
     }
 }
---
+++ ConsoleApp1/leetcode/Two_Pointers/TwoSum_II_InputArrayIsSorted.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.Two_Pointers
+{
+    internal class TwoSum_II_InputArrayIsSorted
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == target)
+                    {
+                        return new int[] { i + 1, j + 1 };
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
