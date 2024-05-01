da4419c4a72c5c0d002708a674036b8933bc913b
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-05-01 11:46:19 +0200

    IsSubsequence finished

---
+++ ConsoleApp1/leetcode/Two_Pointers/IsSubsequenceClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.leetcode.Two_Pointers
+{
+    internal class IsSubsequenceClass
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0)
+            {
+                return true;
+            }
+            for (int i = 0, j = 0; i < t.Length && j < s.Length; i++)
+            {
+                if (s[j] == t[i])
+                {
+                    if (j == s.Length - 1)
+                    {
+                        return true;
+                    }
+                    j++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
