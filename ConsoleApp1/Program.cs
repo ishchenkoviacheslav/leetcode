c86c382820266beaa1e586f36824f28f2bf46f6c
Slava Ishchenko <sishchenko@Pridisbv.onmicrosoft.com>
2024-05-01 17:24:32 +0200

    Container With Most Water finished

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,6 @@
 ﻿class Program
 {
-    static public int MaxArea(int[] height)
-    {
-        int left = 0, right = height.Length - 1;
-        Dictionary<int, int> matrix = new Dictionary<int, int>();
-        while (left < right)
-        {
-            matrix
-            left++;
-            right--;
-        }
-        return 0;
-    }
     static async Task Main()
     {
-        
     }
 }
--- ConsoleApp1/leetcode/Two_Pointers/ContainerWithMostWater.cs
+++ ConsoleApp1/leetcode/Two_Pointers/ContainerWithMostWater.cs
@@ -8,5 +8,30 @@ namespace ConsoleApp1.leetcode.Two_Pointers
 {
     internal class ContainerWithMostWater
     {
+        public int MaxArea(int[] height)
+        {
+            int result = 0;
+            int left = 0;
+            for (int i = 0; i < height.Length - 1; i++)
+            {
+                if (height[i] < left)
+                {
+                    continue;
+                }
+                else
+                {
+                    left = height[i];
+                }
+                for (int j = i + 1; j < height.Length; j++)
+                {
+                    if (Math.Min(height[i], height[j]) * (j - i) > result)
+                    {
+                        result = Math.Min(height[i], height[j]) * (j - i);
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
